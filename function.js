aa777c3b3e11942501eb6bec094c83322df04fa9
namdao236 <namdao@subiz.com>
2022-12-03 12:14:04 +0700

    add function

---
+++ function.js
@@ -0,0 +1,4 @@
+let $interative = document.getElementById('interative');
+let $promote = document.getElementById('promote');
+let $potential = document.getElementById('potential');
+let $manage = document.getElementById('manage');
