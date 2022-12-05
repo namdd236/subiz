84881bfd6a870fd5736f2b345e9eb61b08613253
namdao236 <namdao@subiz.com>
2022-12-05 17:20:17 +0700

    commit

--- function.js
+++ function.js
@@ -1,4 +1,21 @@
-let $interative = document.getElementById('interative');
-let $promote = document.getElementById('promote');
-let $potential = document.getElementById('potential');
-let $manage = document.getElementById('manage');
+// var interative = document.getElementById("interative");
+// var promote = document.getElementById("promote");
+// var potential = document.getElementById("potential");
+// var manage = document.getElementById("manage");
+
+// var option = [
+//     {id: 'interative', background: ''}
+// ]
+
+// var status_option = "interative";
+
+// function changeStatusOption(option) {
+//   this.status_option = option;
+//   document.getElementById(`${option}`).classList.add(`${option}_back`)
+//   document.getElementById(`img_${option}_active`).classList.add("inactive")
+//   document.getElementById(`img_${option}_inactive`).classList.add("active")
+//   document.getElementById(status_option).classList.add("active");
+//   document.getElementById(status_option).classList.remove("inactive");
+// }
+// console.log(document.getElementById('interative'), "F", this.interative);
+// console.log(document.getElementsByClassName('background_icon'), 'OK')
